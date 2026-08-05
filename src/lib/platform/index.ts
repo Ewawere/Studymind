4883dafac2ea59e566bfa18f9f9e2c596f88a25d
Ewawere <153601310+Ewawere@users.noreply.github.com>
2026-08-05 17:17:43 +0100

    Platform layer public API

---
+++ src/lib/platform/index.ts
@@ -0,0 +1,5 @@
+export { on, publish } from "./events";
+export type { DomainEvent, DomainEventType } from "./events";
+export { cached, invalidate, cacheKey } from "./cache";
+export { platformConfig } from "./config";
+export { inc, getMetrics, resetMetrics } from "./metrics";
